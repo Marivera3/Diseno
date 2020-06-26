bc0086fb38a2e12e0090a6e74d8268444c955152
Max <marivera3@uc.cl>
2020-06-26 07:30:30 -0400

    Thread para frame processing

---
+++ Main_Facial+Server+Save/FrameProcessing.py
@@ -0,0 +1,36 @@
+import threading
+from functions_v4 import acquire_frame, draw_frame, show_frame, train
+from imutils.video import FPS
+from queue import Queue
+
+class FrameProcessing(threading.Thread):
+
+    def __init__(self, name='frame processing', queue_video, queue_frame, detector, embedder, frame , recognizer, le):
+        super().__init__(name=name)
+        self.queue_video    = queue_video
+        self.stopped        = False
+        self.fps            = FPS().start()
+        self.queue_frames   = queue_frame
+        self.detector       = detector
+        self.embedder       = embedder
+        self.frame          = frame
+        self.recognizer     = recognizer
+        self.le             = le
+
+    def run(self):
+        self.stopped = False
+        print('[INFO] Thread started: {}...'.format(threading.current_thread().name))
+        while not self.stopped:
+            if self.queue_video.qsize() > 0:
+                frame = self.queue_video.get()
+                face_data = acquire_frame(self.detector, self.embedder, frame , self.recognizer, self.le, 0.5, 0.65)
+                self.queue_frames.put(face_data)
+                self.fps.update()
+
+
+    def stop(self):
+        self.fps.stop()
+        print("[INFO] elasped time: {:.2f}".format(self.fps.elapsed()))
+        print("[INFO] approx. FPS: {:.2f}".format(self.fps.fps()))
+
+        self.stopped = True
