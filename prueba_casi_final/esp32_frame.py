84701074332d3e390278ac1a8a1a967d3d611af9
Agustin Campeny <arcampeny@uc.cl>
2020-07-14 23:47:34 -0400

    esp322

---
+++ prueba_casi_final/esp32_frame.py
@@ -0,0 +1,28 @@
+import socket
+from PIL import Image
+import io
+
+def esp32_frame(host, port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(5)
+    ip = socket.gethostbyname(host)
+    server_address = (ip, port)
+    try:
+        s.connect(server_address)
+    except socket.timeout:
+        return None;
+
+    image = b''
+    buf_len = int.from_bytes(s.recv(2), "little")
+    print(buf_len)
+    while True:
+        image += s.recv(1024)
+        if len(image) >= buf_len:
+            break
+    # Convert list to bytes
+    if len(image) > 1000:
+        img = Image.open(io.BytesIO(image))
+        return img
+
+if __name__ == "__main__":
+    esp32_frame("190.162.132.149", 1228)
